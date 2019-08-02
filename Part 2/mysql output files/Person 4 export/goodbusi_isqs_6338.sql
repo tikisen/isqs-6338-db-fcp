a9f10fe681e1df6679ee1a8413108d9e11b9166a
smcquade86 <53584570+smcquade86@users.noreply.github.com>
2019-08-01 21:26:48 -0400

    Update goodbusi_isqs_6338.sql

--- Part 2/mysql output files/Person 4 export/goodbusi_isqs_6338.sql
+++ Part 2/mysql output files/Person 4 export/goodbusi_isqs_6338.sql
@@ -4852,6 +4852,17 @@ CREATE TABLE IF NOT EXISTS `type_gov` (
   PRIMARY KEY (`tg_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO D50_G03_OLTP.type_gov (tg_id, tg_name, tg_desc)
+SELECT DISTINCT MID(IDCENSUS,3,1), MID(IDCENSUS,3,1), CASE WHEN MID(IDCENSUS,3,1) = 0 
+THEN 'State Dependent School System' WHEN MID(IDCENSUS,3,1) = 1 
+THEN 'County Dependent School System' WHEN MID(IDCENSUS,3,1) = 1 
+THEN 'County Dependent School System' WHEN MID(IDCENSUS,3,1) = 2 
+THEN 'City Dependent School System' WHEN MID(IDCENSUS,3,1) = 3 
+THEN 'Township Dependent School System' WHEN MID(IDCENSUS,3,1) = 5 T
+HEN 'Independent School System' END
+FROM FCP_Dataset.full_dataset
+ORDER BY 1 ASC
+
 --
 -- Constraints for dumped tables
 --
